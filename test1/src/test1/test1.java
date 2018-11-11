6395870accdd18581d0e303108383cd422edf9d9
HUA <HUA@DESKTOP-O2TNPQL>
2018-11-11 19:51:51 +0800

    test commit

---
+++ test1/src/test1/test1.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package test1;
+
+/**
+ * @author HUA
+ *
+ */
+public class test1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("123");
+
+	}
+
+}
