057c6204f3ba4d4a05ba0a7c02e5ec6c1e6614d0
lhanxin0 <leehx-pa16@student.tarc.edu.my>
2018-11-28 13:06:10 +0800

    Fixed travis.yml

--- FlowerSystem/src/Catalog_Order/CatOrder.java
+++ FlowerSystem/src/Catalog_Order/CatOrder.java
@@ -19,7 +19,7 @@ import java.util.concurrent.ArrayBlockingQueue;
  * @author Han Xin
  */
 public class CatOrder {
-    //Testing1
+    //Testing12
 
     /**
      * @param args the command line arguments
