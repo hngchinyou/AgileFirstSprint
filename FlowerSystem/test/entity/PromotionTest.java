59e2a8ba0ffc4e7d6ee81f6aecf978902919d28b
user <user@DESKTOP-0EGLPL9>
2018-12-10 22:25:29 +0800

    [finishes #161834741] [#161834745]

--- FlowerSystem/test/entity/PromotionTest.java
+++ FlowerSystem/test/entity/PromotionTest.java
@@ -155,6 +155,7 @@ public class PromotionTest {
         p.setAmount(amount);
         // TODO review the generated test code and remove the default call to fail.
         //fail("The test case is a prototype.");
+        
     }
 
     /**
