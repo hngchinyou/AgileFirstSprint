9e054ad2ab996b8197a8e7df9b4573c0d182cdf9
lhanxin0 <leehx-pa16@student.tarc.edu.my>
2018-12-11 19:37:50 +0800

    Junit

---
+++ FlowerSystem/test/customized/CustFloArrangeTest.java
@@ -0,0 +1,33 @@
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//package customized;
+//
+//import entity.Customer;
+//import entity.CustomizedFlower;
+//import entity.Flower2;
+//import java.util.ArrayList;
+//import java.util.List;
+//import org.junit.Before;
+//import org.junit.Test;
+//import static org.junit.Assert.*;
+//
+///**
+// *
+// * @author User
+// */
+//public class CustFloArrangeTest {
+//    
+//    public CustFloArrangeTest() {
+//    }
+//    
+//    @Before
+//    public void setUp() {
+//    }
+//
+//   
+//   
+//    
+//}
